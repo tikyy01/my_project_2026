0d07e1c29f3c4bfef3ea5b851a263dc99a4dc7e4
tikyy01 <ervinaksizov@gmail.com>
2026-05-01 00:41:05 +0300

    my_4.3

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -4,7 +4,7 @@
       <v-col cols="12" sm="8" lg="6">
         <v-card class="elevation-12">
           <v-toolbar dark color="primary">
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>Registration</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
             <v-form v-model="valid" ref="form" validation>
@@ -25,6 +25,14 @@
                 v-model="password"
                 :rules="passwordRules"
               ></v-text-field>
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="confirm-password"
+                label="Confirm Password"
+                type="password"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+              ></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
@@ -34,7 +42,7 @@
               @click="onSubmit"
               :disabled="!valid"
             >
-              Login
+              Create Account
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -49,6 +57,7 @@ export default {
     return {
       email: "",
       password: "",
+      confirmPassword: "",
       valid: false,
       emailRules: [
         v => !!v || 'E-mail is required',
@@ -57,6 +66,10 @@ export default {
       passwordRules: [
         v => !!v || 'Password is required',
         v => (v && v.length >= 6) || 'Password must be more or equal than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match'
       ]
     }
   },
