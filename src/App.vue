077fd1abccb140d468cc70644520da7141c7657c
tikyy01 <ervinaksizov@gmail.com>
2026-04-30 23:29:46 +0300

    my_2.5

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item
+          v-for="link in links"
+          :key="link.title"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
+        <v-btn
+          text
+          v-for="link in links"
+          :key="link.title"
+        >
           <v-icon
             start
-            icon="mdi-cake-variant"
+            :icon="link.icon"
           ></v-icon>
-          Link One
+          {{ link.title }}
         </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
     <v-main>
     </v-main>
   </v-app>
@@ -23,6 +50,17 @@
 
 <script>
 export default {
-  name: 'App'
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        { title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders" },
+        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
+      ]
+    }
+  }
 }
 </script>
