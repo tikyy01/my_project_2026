38cb761fc679fd1797deb62001461700ec4aa206
tikyy01 <ervinaksizov@gmail.com>
2026-04-30 23:15:04 +0300

    my_2.2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-<h1>Очистили стартовый проект</h1>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <v-icon
+            start
+            icon="mdi-cake-variant"
+          ></v-icon>
+          Link One
+        </v-btn>
+        <v-btn>Link Two</v-btn>
+        <v-btn>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
+  name: 'App'
 }
 </script>
