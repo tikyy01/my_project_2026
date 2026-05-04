06c6d634664c44c153b5d404976980a15bfdc4f9
tikyy01 <ervinaksizov@gmail.com>
2026-05-04 19:07:32 +0300

    9

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -1,9 +1,32 @@
-﻿<template>
+<template>
   <v-container>
-    <v-row justify="center">
+    <v-row>
       <v-col cols="12">
-        <h1>Ad Details Page</h1>
-        <p>Ad ID: ' + '$route.params.id' + '</p>
+        <v-card class="mt-5">
+          <v-img
+            height="300px"
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            cover
+          ></v-img>
+
+          <v-card-text>
+            <h1 class="text--primary mb-3">Lorem Ipsum</h1>
+            <p>
+              Lorem Ipsum has been the industry's unknown printer took a galley of type and 
+              scrambled it to make a type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset sheets containing 
+              Lorem Ipsum passages, and more recently with desktop publishing software like 
+              Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text class="warning">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -11,6 +34,9 @@
 
 <script>
 export default {
-  name: 'AdView'
+  name: 'AdView',
+  data() {
+    return {}
+  }
 }
 </script>
