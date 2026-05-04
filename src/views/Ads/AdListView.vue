fafc5b48dd9882f2133d16aa8c891fa7d06a989a
tikyy01 <ervinaksizov@gmail.com>
2026-05-04 19:05:39 +0300

    8

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,8 +1,40 @@
-﻿<template>
+<template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="12">
-        <h1>My Ads List</h1>
+      <v-col cols="12" sm="8" lg="6">
+        <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+        
+        <v-card 
+          v-for="ad in ads" 
+          :key="ad.id" 
+          class="mb-3" 
+          max-width="1000"
+        >
+          <v-row>
+            <v-col cols="4">
+              <v-img 
+                :src="ad.src" 
+                height="175px" 
+                cover
+              ></v-img>
+            </v-col>
+            
+            <v-col cols="8">
+              <h2 class="text--primary">{{ ad.title }}</h2>
+              <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  color="primary" 
+                  variant="text" 
+                  :to="'/ad/' + ad.id"
+                >
+                  Open
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -10,6 +42,40 @@
 
 <script>
 export default {
-  name: 'AdListView'
+  name: 'AdListView',
+  data() {
+    return {
+      ads: [
+        {
+          title: "First",
+          desc: "First Desc. This is a longer description to test how the text overflow and ellipsis works in the card component. It should be truncated after 85 pixels.",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1"
+        },
+        {
+          title: "Second",
+          desc: "Second Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          id: "2"
+        },
+        {
+          title: "Third",
+          desc: "Third Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id: "3"
+        },
+        {
+          title: "Fourth",
+          desc: "Fourth Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "4"
+        }
+      ]
+    }
+  }
 }
 </script>
